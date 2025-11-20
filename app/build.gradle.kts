be6ef0c379f5186cc23b24729a3fdb05223a69b9
Petra Satkova <xsatkova@mendelu.cz>
2025-11-20 21:27:36 +0100

    gradle, launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,26 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.kotlin.serialization)
 }
 
+val properties = Properties()
+properties.load(project.rootProject.file("local.properties").reader())
+
+val server = properties.getProperty("server")
+
 android {
+    val versionMajor = 0
+    val versionMinor = 0
+    val versionPatch = 1
+    val myVersionCode = versionMajor * 10000 + versionMinor * 100 + versionPatch
+    val myVersionName = "${versionMajor}.${versionMinor}.${versionPatch}"
+
     namespace = "cz.mendelu.souvenirbox"
     compileSdk = 36
 
@@ -12,9 +28,10 @@ android {
         applicationId = "cz.mendelu.souvenirbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = myVersionCode
+        versionName = myVersionName
 
+        // TODO change to mine
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -26,6 +43,9 @@ android {
                 "proguard-rules.pro"
             )
         }
+        debug {
+            buildConfigField(type = "String", name = "SERVER_URL", value = server)
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -36,6 +56,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -56,4 +77,40 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Navigation
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.navigation.compose)
+    implementation(libs.kotlin.serialization.json)
+
+    // Datastore
+    implementation(libs.datastore.core)
+    implementation(libs.datastore.preferences)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.compose)
+    ksp(libs.hilt.compiler.ksp)
+
+    // Moshi
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+    ksp(libs.moshi.ksp)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
+    implementation(libs.retrofit.okhtt3)
+
+    // coil
+    implementation(libs.coil)
+
+    // Splashscreen
+    implementation(libs.splashscreen)
+
+    // Testing hilt
+    androidTestImplementation(libs.hilt.android.testing)
+    kspAndroidTest(libs.hilt.android.compiler)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
